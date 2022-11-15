46465786775714621e3c69c940137264e1f5f836
yozhikes <106804366+yozhikes@users.noreply.github.com>
2022-11-15 22:35:55 +0300

    Add files via upload

---
+++ HotToursWNuget/HotToursLogic/Logic.cs
@@ -0,0 +1,24 @@
+﻿using HotToursLogic.Models;
+namespace HotToursLogic
+{
+    public class Logic
+    {
+        private readonly List<Tour> tours = new();
+        public List<Tour> Tours
+        {
+            get { return tours; }
+        }
+        public void Add(Tour tour)
+        {
+            tours.Add(tour);
+        }
+        public void Edit(Tour tour, int i)
+        {
+            tours[i] = tour;
+        }
+        public void Delete(Tour tour)
+        {
+            tours.Remove(tour);
+        }
+    }
+}
